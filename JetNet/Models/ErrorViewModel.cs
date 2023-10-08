dadcf21d1704df6ccfc5c965f4721c66b21408a0
User <User@DESKTOP-KQTTIK9>
2023-10-08 10:36:41 +0300

    Add project files.

---
+++ JetNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JetNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
